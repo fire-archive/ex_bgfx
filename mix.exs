ebf3fe276dc16a35761f4b380ccb32c515716182
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2015-12-02 20:25:53 -0800

    Let's try this change to mix: change mix so that uses the build folder as the relative path.

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule ExBgfx.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [mod: {Bgfx, []}, 
-    applications: [ :logger]]
+     applications: [ :logger]]
   end
 
   # Dependencies can be Hex packages:
@@ -40,12 +40,9 @@ defmodule Mix.Tasks.Compile.Bgfx do
   def run(_) do
     if match? {:win32, _}, :os.type do
       starting_dir = System.cwd()
-      case :code.priv_dir(:bgfx) do
-        {:error, _error_code} -> install_prefix = :filename.join(:filename.dirname(Ebin), "priv") 
-          #priv -> working_dir = :filename.join(:filename.dirname(Ebin), "priv") 
-          _ -> install_prefix = to_string :code.priv_dir(:bgfx)
-      end
-      working_dir = install_prefix <> "../build"
+      install_prefix = Mix.Project.build_path <> "/lib/bgfx/priv" 
+      working_dir = Mix.Project.build_path <> "/build"
+      File.mkdir(install_prefix)
       File.mkdir(working_dir)
       File.cd(working_dir)
       {result, _error_code} = System.cmd("cmake", ["-GVisual Studio 14 2015 Win64", "-DCMAKE_INSTALL_PREFIX=" <> install_prefix, "-DCMAKE_SYSTEM_VERSION=10.0", starting_dir], stderr_to_stdout: true) 
@@ -53,10 +50,10 @@ defmodule Mix.Tasks.Compile.Bgfx do
       {result, _error_code} = System.cmd("cmake", ["--build", ".", "--target", "install"], stderr_to_stdout: true) 
       IO.binwrite result
       File.cd(starting_dir)
-    else
-      {result, _error_code} = System.cmd("make", ["priv/bgfx.so"], stderr_to_stdout: true)
-      IO.binwrite result
-    end
+      else
+       {result, _error_code} = System.cmd("make", ["priv/bgfx.so"], stderr_to_stdout: true)
+       IO.binwrite result
+      end
     
     :ok
   end
